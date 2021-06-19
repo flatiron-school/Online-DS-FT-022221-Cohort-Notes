9b7916143cd2fba8080014cb0fae38162f5a3c71
jirvingphd <james.irving.phd@gmail.com>
2021-06-19 15:45:25 -0400

    Update ts_functions.py

--- Phase_4/topic_38_time_series_models/ts_functions.py
+++ Phase_4/topic_38_time_series_models/ts_functions.py
@@ -4,6 +4,7 @@ import statsmodels.tsa.api as tsa
 import pandas as pd
 import matplotlib.pyplot as plt
 from IPython.display import display
+
 def adfuller_test_df(ts,index=['AD Fuller Results']):
     """Returns the AD Fuller Test Results and p-values for the null hypothesis
     that there the data is non-stationary (that there is a unit root in the data)"""
@@ -56,3 +57,103 @@ def stationarity_check(TS,window=8,plot=True,index=['AD Fuller Results']):
         
     return dftest
     
+    
+def plot_acf_pacf(ts,figsize=(9,6),lags=52,suptitle=None,sup_y = 1.01):
+    """Plot pacf and acf using statsmodels"""
+    fig,axes=plt.subplots(nrows=2,figsize=figsize)
+    
+    tsa.graphics.plot_acf(ts,ax=axes[0],lags=lags);
+    tsa.graphics.plot_pacf(ts,ax=axes[1],lags=lags);
+    
+    ## Add grid
+    [ax.grid(axis='x',which='both') for ax in axes]
+    
+    if suptitle is not None:
+        fig.suptitle(suptitle,y=sup_y,fontweight='bold',fontsize=15)
+        
+    fig.tight_layout()
+    return fig,axes
+
+
+## funtionize diagnosing
+def diagnose_model(model):
+    """Takes a fit statsmodels model and displays the .summary 
+    and plots the built-in plot.diagnostics()"""
+    display(model.summary())
+    model.plot_diagnostics()
+    plt.tight_layout()
+    
+    
+def get_df_from_pred(forecast_or_pred,forecast_label='Forecast'):
+    """Takes a PredictionResultsWrapper from statsmodels
+    extracts the confidence intervals and predicted mean and returns in a df"""
+    forecast_df = forecast_or_pred.conf_int()
+    forecast_df.columns = ['Lower CI','Upper CI']
+    forecast_df[forecast_label] = forecast_or_pred.predicted_mean
+    return forecast_df
+
+def plot_forecast_from_df(forecast_df,ts_diff=None,orig_label='True Data',
+                          forecast_label='Forecast',
+                          last_n_lags=52,figsize=(10,4)):
+    """Takes a forecast_df from get_df_from_pred and optionally 
+    the training/original time series.
+    
+    Plots the original ts, the predicted mean and the 
+    confidence invtervals (using fill between)"""
+    fig,ax = plt.subplots(figsize=figsize)
+
+    if ts_diff is not None:
+        ts_diff.iloc[-last_n_lags:].plot(label='True Data')
+        
+   
+    forecast_df['Forecast'].plot(ax=ax,label=forecast_label)
+    ax.fill_between(forecast_df.index,
+                    forecast_df['Lower CI'], 
+                    forecast_df['Upper CI'],color='g',alpha=0.3)
+    ax.legend()
+    ax.set(title=f'Forecasted {ts_diff.name}')
+    return fig,ax\
+        
+        
+        
+### FORECAST SPECIFIC FUNCTIONS
+
+def get_forecast(model,steps=12):
+    pred = model.get_forecast(steps=steps)
+    forecast = pred.conf_int()
+    forecast.columns = ['Lower CI','Upper CI']
+    forecast['Forecast'] = pred.predicted_mean
+    return forecast
+
+    
+def plot_forecast(model,ts,last_n_lags=52,future_steps=12):
+    forecast_df = get_forecast(model,steps=future_steps)
+
+    fig,ax = plt.subplots(figsize=(12,5))
+    ts.iloc[-last_n_lags:].plot(label='True Data')
+    forecast_df['Forecast'].plot(ax=ax)
+    ax.fill_between(forecast_df.index,
+                    forecast_df['Lower CI'], forecast_df['Upper CI'],alpha=0.6)
+    ax.legend()
+    ax.set(title=f'Forecasted {ts.name}')
+    return fig,ax
+
+
+# def evaluate_model(model,ts,last_n_lags =52,steps=12):
+#     display(model.summary())
+#     model.plot_diagnostics();
+#     fig,ax=plot_forecast(model,ts,future_steps=steps,last_n_lags=last_n_lags)
+#     return fig,ax
+
+def evaluate_model(model,ts,test_ts=None, last_n_lags =52,steps=12):
+    diagnose_model(model)
+    
+    forecast = model.get_forecast(steps=steps)
+    forecast_df = get_df_from_pred(forecast,)
+    
+    fig, ax = plot_forecast_from_df(forecast_df,ts_diff=ts,
+                                    last_n_lags=last_n_lags)
+    
+    if test_ts:
+        test_ts.plot(ax=ax)
+    return fig,ax
